57abc9e56cb7d3e519abb2074a947060af5f6add
yungsem <yungsem@126.com>
2020-06-13 17:26:09 +0800

    feat: add  strign

--- strings/strings.go
+++ strings/strings.go
@@ -1,6 +1,10 @@
 package strings
 
-import "strings"
+import (
+	"crypto/md5"
+	"fmt"
+	"strings"
+)
 
 func LowerCaseFirstLetter(s string) string {
 	if len(s) == 0 {
@@ -9,4 +13,9 @@ func LowerCaseFirstLetter(s string) string {
 	first := s[0:1]
 	rest := s[1:]
 	return strings.ToLower(first) + rest
+}
+
+func MD5(s string) string {
+	has := md5.Sum([]byte(s))
+	return fmt.Sprintf("%x", has)
 }
